b2aabadfaf1f1bf631b0d4db2989d762c99ec940
cjparsonson <cjparsonson@gmail.com>
2025-09-04 22:03:22 +0100

    Update - Add movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_SPAWN_RATE
+from player import Player
 
 def main():
     pygame.init()
@@ -10,6 +11,8 @@ def main():
     # Initialize clock and dt
     clock = pygame.time.Clock()
     dt = 0
+    # Initialize player
+    player =  Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     # Game loop
     while True:
         for event in pygame.event.get():
@@ -17,8 +20,13 @@ def main():
                 pygame.quit()
                 return
         screen.fill((0, 0, 0))
+        # Render player
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # Limit to 60 FPS and get delta time
+
+
 
 
 if __name__ == "__main__":
